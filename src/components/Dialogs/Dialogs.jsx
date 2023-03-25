e191878d5f6351b9dbc5f1787f479b14574dfbb5
Dmitriy Sindeev <d.s.sindeev@bk.ru>
2023-03-25 18:03:48 +0300

    leson #40

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { sendMessageCreator, updateNewMessageTextCreator } from "../../redux/state";
 import Chat from "./Chat/Chat";
 import s from './Dialogs.module.css';
 import Message from "./Messages/Message";
 
 const Dialogs = (props) => {
 
-    let dialogsElement = props.state.dialogs.map ( d => <Chat name={d.name} id={d.id} /> )
-    let messagesElement = props.state.messages.map ( m => <Message message= {m.message} id= {m.id} />)
+    let state = props.store.getState().dialogsPage;
+
+    let dialogsElement = state.dialogs.map(d => <Chat name={d.name} id={d.id} />)
+
+    let messagesElement = state.messages.map(m => <Message message={m.message} id={m.id} />)
+
+    let newMessageText = state.newMessageText;
+
+    let onSendMessageClick = () => {
+        props.store.dispatch(sendMessageCreator())
+    }
+
+    let onNewMessageChange = (e) => {
+        let body = e.target.value;
+        props.store.dispatch(updateNewMessageTextCreator(body))
+    }
 
     return (
         <div className={s.dialogs}>
@@ -14,7 +29,17 @@ const Dialogs = (props) => {
                 {dialogsElement}
             </div>
             <div className={s.messages}>
-            {messagesElement}
+                <div>
+                    {messagesElement}
+                </div>
+                <div>
+                    <div>
+                        <textarea onChange={onNewMessageChange} value={newMessageText}></textarea>
+                    </div>
+                    <div>
+                        <button onClick={onSendMessageClick}>Send Message</button>
+                    </div>
+                </div>
             </div>
 
         </div>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,10 @@
 const ADD_POST = 'ADD-POST';
+
 const UPDATE_NEW_POST = 'UPDATE-NEW-POST';
+
+const SEND_MESSAGE = 'SEND_MESSAGE';
+
+const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE_NEW_MESSAGE_TEXT';
 
 let store = {
     _state: {
@@ -7,7 +12,7 @@ let store = {
             posts: [
                 { id: 1, message: 'Hi, my name is Dima. How are you?', likeCount: 120 },
                 { id: 2, message: 'My photo before how i can big star', likeCount: 10235 }],
-            newPostText: 'lolka'
+            newPostText: ''
         },
         dialogsPage: {
             dialogs: [
@@ -25,7 +30,8 @@ let store = {
                 { id: 3, message: 'Give me your pasport pls' },
                 { id: 4, message: 'Lets go mazafaka' },
                 { id: 5, message: 'Yo' }
-            ]
+            ],
+            newMessageText: ''
         }
     },
 
@@ -54,14 +60,28 @@ let store = {
         } else if (action.type === UPDATE_NEW_POST) {
             this._state.proffilePage.newPostText = action.newText;
             this._callSubscriber(this._state);
+        } else if (action.type === SEND_MESSAGE) {
+            let newMessage = { 
+                id: 6, 
+                message: this._state.dialogsPage.newMessageText 
+            }
+            this._state.dialogsPage.messages.push(newMessage)
+            this._state.dialogsPage.newMessageText = ''
+            this._callSubscriber(this._state);
+        } else if (action.type === UPDATE_NEW_MESSAGE_TEXT) {
+            this._state.dialogsPage.newMessageText = action.body
+            this._callSubscriber(this._state);
         }
     }
 }
 
- export const addPostActionCreator = () => ({ type: ADD_POST })
-
+export const addPostActionCreator = () => ({ type: ADD_POST })
 
 export const updatePostActionCreator = (text) => ({ type: UPDATE_NEW_POST, newText: text })
+
+export const sendMessageCreator = () => ({ type: SEND_MESSAGE })
+
+export const updateNewMessageTextCreator = (body) => ({ type: UPDATE_NEW_MESSAGE_TEXT, body: body })
 
 window.store = store;
 
